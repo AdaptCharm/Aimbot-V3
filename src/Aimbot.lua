5e9abb0f34cd8c89a16ca26951754ede7b745559
Jamie Isaksen <jamie@jamie.no>
2024-09-01 21:11:46 +0200

    Update Aimbot.lua

--- src/Aimbot.lua
+++ src/Aimbot.lua
@@ -112,8 +112,8 @@ getgenv().ExunysDeveloperAimbot = {
 		LockMode = 1, -- 1 = CFrame; 2 = mousemoverel
 		LockPart = "Head", -- Body part to lock on
 
-		TriggerKey = "Q",
-		Toggle = true
+		TriggerKey = Enum.KeyCode.Q,
+		Toggle = false
 	},
 
 	FOVSettings = {
